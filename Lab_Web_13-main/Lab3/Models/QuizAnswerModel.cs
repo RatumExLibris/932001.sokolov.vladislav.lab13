ecc7c77cde8d1e2a027de5c2b2aac7fd749cac30
Vladislav Sokolov <coko.2002@mail.ru>
2023-01-16 17:26:48 +0700

    construct

--- Lab_Web_13-main/Lab3/Models/QuizAnswerModel.cs
+++
@@ -1,8 +0,0 @@
-﻿using Lab3.Utils;
-
-namespace Lab3.Models {
-    public class QuizAnswerModel {
-        [LessThanOrNull(101, ErrorMessage = "Здесь не может быть такого большого числа")]
-        public int? Answer { get; set; }
-    }
-}
